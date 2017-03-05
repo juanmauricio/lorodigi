cb05b43b7b128b1e95cff058a501321d5b2a89be
juanmauricio <juan.mauricio.lopez@gmail.com>
2017-03-04 23:01:23 -0500

    facebook get

--- modules/identity/server/controllers/facebookgraph.server.controller.js
+++ modules/identity/server/controllers/facebookgraph.server.controller.js
@@ -5,22 +5,46 @@
  */
 var path = require('path'),
   mongoose = require('mongoose'),
-  Identity = mongoose.model('Identity'),
+  User = mongoose.model('User'),
   errorHandler = require(path.resolve('./modules/core/server/controllers/errors.server.controller')),
-  graph = require('fbgraph');
+  graph = require('fbgraph'),
+  config = require(path.resolve('./config/config'));
 
 
 /**
  * Get facebook variables to calculate score for an specific user.
  */
-exports.getfacebookvariables = function(req, res) 
-{
-  var user = req.user;
-  var access_token = req.user.AdditionalProvidersData["facebook"].access_token;
+exports.getfacebookvariables = function (req, res) {
 
-  graph.setAccessToken(access_token);
+  User.findOne({
+    _id: req.user._id
+  }).exec(function (err, user) {
+    if (err) {
+      return next(err);
+    } else if (!user) {
+      return next(new Error('Failed to load User ' + id));
+    }
 
-// config.facebook.clientID
-// config.facebook.clientSecret
+    getFacebookVariables(user, config);
+  });
+
+
+  function getFacebookVariables(user, config) {
+    var access_token = user.additionalProvidersData.facebook.accessToken;
+    graph.setAccessToken(access_token);
+    graph.setAppSecret(config.facebook.clientSecret)
+
+  // pass it in as part of the url
+    graph.post("129954010860422" + "/feed?access_token=" + access_token,{message: "prueba"}, function(err, res) {
+        // returns the post id
+        console.log(res); // { id: xxxxx}
+    })
+
+  }
+
+
+
+  // config.facebook.clientID
+  // config.facebook.clientSecret
 
 }
